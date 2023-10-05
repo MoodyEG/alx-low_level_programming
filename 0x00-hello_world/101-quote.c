6a202971e6fffac09b5f1a94aa78e77b17803cca
MoodyEG <moodyeg2011@yahoo.com>
2023-10-05 15:28:34 -0700

    done fast

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+include <unistd.h>
+/**
+ * main - Entry point
+ * I know how it works, i just don't know the numbers and libiray in it
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1);
+}
+
