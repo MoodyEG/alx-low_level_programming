ac99fbd9ee144bc3e0945453549ab4aa5ba1e884
MoodyEG <moodyeg2011@yahoo.com>
2023-10-05 07:01:41 -0700

    done fast

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-int main(void) /*time to test*/
+/*time to test*/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-		return 0;}
+		return 0; }
