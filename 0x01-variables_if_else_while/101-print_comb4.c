33d8817e8795be163e605930827186e1f4987f5d
MoodyEG <moodyeg2011@yahoo.com>
2023-10-07 14:26:03 -0700

    done fast

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int i;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = a + 1; b < 10; b++)
+		{
+			for (i = a + 1; i < 10; i++)
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(i + '0');
+				if (a != 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
