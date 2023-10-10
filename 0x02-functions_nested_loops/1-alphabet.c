aba41963dc380ff92489ee96b4fcb2b6bbd7d8a0
MoodyEG <moodyeg2011@yahoo.com>
2023-10-10 09:26:16 -0700

    done fast

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 'a',a < 'z',a++)
+	{
+		print_alphabet('a');
+	}
+	_putchar('\n');
+
+	return (0);
+}
