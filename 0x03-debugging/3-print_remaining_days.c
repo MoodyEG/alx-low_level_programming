c08e6461db8ad63aab68424e6a20906fed7f9c51
MoodyEG <moodyeg2011@yahoo.com>
2023-10-11 03:49:03 -0700

    done fast

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -18,15 +18,9 @@ void print_remaining_days(int month, int day, int year)
 		{
 			day++;
 		}
-		if (day >= 366)
-		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-		}
-		else
-		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 366 - day);
-		}
+
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
@@ -36,15 +30,8 @@ void print_remaining_days(int month, int day, int year)
 		}
 		else
 		{
-			if (day >= 365)
-			{
-				printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-			}
-			else
-			{
-				printf("Day of the year: %d\n", day);
-				printf("Remaining days: %d\n", 365 - day);
-			}
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
 }
