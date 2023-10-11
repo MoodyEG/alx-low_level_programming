014ca65fe29adb9b05c4fbcf46eb335e436fa5cb
MoodyEG <moodyeg2011@yahoo.com>
2023-10-10 23:35:52 -0700

    done fast

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -13,15 +13,11 @@ int main(void)
 	a = 0;
 	for (n = 0; n < 1024; n++)
 	{
-		if ((n / 3) % 10 == 0)
+		if (n % 3 == 0 || n % 5 == 0)
 		{
 			a = a + n;
 		}
-		else if ((n / 5) % 10 == 0)
-		{
-			a = a + n;
-		}
-	}	
+	}
 	printf("%d\n", a);
 
 	return (0);
