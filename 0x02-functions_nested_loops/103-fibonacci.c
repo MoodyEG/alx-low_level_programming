bbe976ce0d995443f838e0dac76e7e3b334ad7ba
MoodyEG <moodyeg2011@yahoo.com>
2023-10-11 00:42:25 -0700

    done fast

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers
+ * Return: always 0
+ */
+int main(void)
+{
+	long a = 1;
+	long b = 2;
+	long i = 0;
+	long n;
+
+	for (n = 0; n < 4000000; n++)
+	{
+		n = a + b;
+		a = b;
+		b = n;
+
+		if (a % 2 == 0)
+		{
+			i = i + a;
+		}
+		
+	}
+	printf("%ld\n", i);
+
+	return (0);
+}
