3d298bd02ecad97446b2a66d0e4d5ec410ed3790
MoodyEG <moodyeg2011@yahoo.com>
2023-10-11 01:12:15 -0700

    done fast

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,28 +2,46 @@
 #include <stdio.h>
 
 /**
- * main - prints the first 50 Fibonacci numbers
+ * main - prints the first 98 Fibonacci numbers
  * Return: always 0
  */
 int main(void)
 {
-	unsigned long a = 1;
-	unsigned long b = 2;
-	int n;
+	long a = 1, b = 2, i, a1, b1, ii;
+	int c, case1, case2;
 
+	case1 = case2 = 1
 	printf("1, 2");
-	for (n = 2; n < 98; n++)
+	for (c = 2; n < 98; n++)
 	{
-		if (n % 2 == 0)
+		if (case1)
 		{
-			a = a + b;
-			printf(", %lu", a);
+		i = a + b;
+		printf(", %ld", i);
+		a = b;
+		b = i;
 		}
 		else
 		{
-			b = a + b;
-			printf(", %lu", b);
+			if (case2)
+			{
+				a1 = a % 1000000000;
+				b1 = b % 1000000000;
+				a = a / 1000000000;
+				b = b / 1000000000;
+				case2 = 0;
+			}
+			ii = (a1 + b1);
+			i = a + b + (ii / 1000000000);
+			printf(", %ld", i);
+			printf("%ld", ii % 1000000000);
+			a = b;
+			a1 = b1;
+			b = i;
+			b1 = (ii % 1000000000);
 		}
+		if (((a + b) < 0) && case1 == 1)
+			case1 = 0;
 	}
 	printf("\n");
 
