ba58301d402caf88679d2a134b08f855f84413ea
MoodyEG <moodyeg2011@yahoo.com>
2023-10-10 23:06:45 -0700

    done fast

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,35 +7,34 @@ void print_times_table(int n)
 {
 	int row, column, a;
 
-	if (n <= 15 && n >= 0) {
-		for (row = 0; row <= n; row++) {
-			for (column = 0; column <= n; column++) {
+	if (n <= 15 && n >= 0)
+	{
+		for (row = 0; row <= n; row++)
+		{
+			_putchar('0');
+			for (column = 0; column <= n; column++)
+			{
 				a = row * column;
 
-				if (column == 0) {
-					_putchar(a + '0');
+				_putchar(',');
+				_putchar(' ');
+				if (a < 10)
+				{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(a % 10 + '0');
 				}
-				else {
-					_putchar(',');
-					_putchar(' ');
-
-					if (a < 100) {
-					if (a < 10) {
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(a % 10 + '0');
-					}
-					else {
-					_putchar(' ');
-					_putchar(a / 10 + '0');
-					_putchar(a % 10 + '0');
-					}
-					}
-					else {
-					_putchar(a / 100 + '0');
-					_putchar((a / 10) % 10 + '0');
-					_putchar(a % 10 + '0');
-					}
+				else if (a < 100)
+				{
+				_putchar(' ');
+				_putchar(a / 10 + '0');
+				_putchar(a % 10 + '0');
+				}
+				else
+				{
+				_putchar(a / 100 + '0');
+				_putchar((a / 10) % 10 + '0');
+				_putchar(a % 10 + '0');
 				}
 			}
 			_putchar('\n');
