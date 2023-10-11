58675d11409b8ac8af6f2cae1c147b55f3cd1d58
MoodyEG <moodyeg2011@yahoo.com>
2023-10-10 23:09:17 -0700

    done fast

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,7 +12,7 @@ void print_times_table(int n)
 		for (row = 0; row <= n; row++)
 		{
 			_putchar('0');
-			for (column = 0; column <= n; column++)
+			for (column = 1; column <= n; column++)
 			{
 				a = row * column;
 
