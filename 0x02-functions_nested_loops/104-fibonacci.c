80634477cab0337ca348439b192ac01834790035
MoodyEG <moodyeg2011@yahoo.com>
2023-10-11 01:17:25 -0700

    done fast

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,9 +10,9 @@ int main(void)
 	long a = 1, b = 2, i, a1, b1, ii;
 	int c, case1, case2;
 
-	case1 = case2 = 1
+	case1 = case2 = 1;
 	printf("1, 2");
-	for (c = 2; n < 98; n++)
+	for (c = 2; c < 98; n++)
 	{
 		if (case1)
 		{
@@ -23,25 +23,25 @@ int main(void)
 		}
 		else
 		{
-			if (case2)
+			if (case2) /*once it reaches the limit we simply try to devide the number into 2*/
 			{
 				a1 = a % 1000000000;
 				b1 = b % 1000000000;
 				a = a / 1000000000;
 				b = b / 1000000000;
-				case2 = 0;
+				case2 = 0; /*will continue case 2 but won't devide anymore*/
 			}
 			ii = (a1 + b1);
-			i = a + b + (ii / 1000000000);
+			i = a + b + (ii / 1000000000); /*for any number goes over that needs to be added*/
 			printf(", %ld", i);
-			printf("%ld", ii % 1000000000);
+			printf("%ld", ii % 1000000000); /*the '%' here so we only take the first 9 numbers only*/
 			a = b;
 			a1 = b1;
 			b = i;
 			b1 = (ii % 1000000000);
 		}
-		if (((a + b) < 0) && case1 == 1)
-			case1 = 0;
+		if (((a + b) < 0) && case1 == 1) /*that means it is out of long range*/
+			case1 = 0; /*will lead us to case 2*/
 	}
 	printf("\n");
 
