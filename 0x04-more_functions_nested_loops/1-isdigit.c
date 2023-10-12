a686225e616b807e5bd82cb2cb3acaeea64136e8
MoodyEG <moodyeg2011@yahoo.com>
2023-10-11 22:57:04 -0700

    done fast

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - check the code.
+ * @c: our main int
+ * 
+ * Return: Returns 1 if c is uppercase, Returns 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
