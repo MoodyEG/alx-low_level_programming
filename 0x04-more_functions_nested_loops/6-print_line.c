2d63834d92ca442ac843037f37d896d67c4ef7af
MoodyEG <moodyeg2011@yahoo.com>
2023-10-12 05:09:38 -0700

    done fast

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal.
+ * @n: our main int
+ */
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar('_');
+		n--
+	}
+	_putchar('\n');
+}
