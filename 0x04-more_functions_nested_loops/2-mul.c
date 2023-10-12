baa6f27647c247285d007014a64d8e39d0338f0f
MoodyEG <moodyeg2011@yahoo.com>
2023-10-12 03:36:18 -0700

    done fast

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,10 @@
 #include "main.h"
+
 /**
  * mul - multiply our numbers
  * @a: our main int
  * @b: our main int
- * 
- * Return: Returns 1 if c is uppercase, Returns 0 otherwise
+ * Return: the answer
  */
 int mul(int a, int b)
 {
