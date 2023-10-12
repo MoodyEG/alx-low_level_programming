885042552ded221bc6a63138f0390c23a3ab76ec
MoodyEG <moodyeg2011@yahoo.com>
2023-10-12 03:54:20 -0700

    done fast

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0-14 ten times
+ */
+void more_numbers(void)
+{
+	int a;
+	int b;
+
+	for (b = 0; b < 11; b++)
+	{
+		for (a = 0; a < 15; a++)
+		{
+			_putchar(a + '0');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
