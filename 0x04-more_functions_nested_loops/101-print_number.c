2ffc3c0e10679bbaa01902314c2875c7ed66a751
MoodyEG <moodyeg2011@yahoo.com>
2023-10-12 10:14:03 -0700

    done fast

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -20,15 +20,14 @@ void print_number(int n)
 	}
 	b = a;
 	c = 1;
-
 	while (b > 9)
 	{
 		b = b / 10;
 		c = c * 10;
 	}
-
-	for (;c >= 1; c = (c / 10))
+	while (c >= 1)
 	{
 		_putchar(((a / c) % 10) + '0');
+		c = c / 10;
 	}
 }
