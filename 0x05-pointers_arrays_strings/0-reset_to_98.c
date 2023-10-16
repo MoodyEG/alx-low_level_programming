396daa6e226ef61ecf71f9f01ee3bb39a070955c
MoodyEG <moodyeg2011@yahoo.com>
2023-10-16 13:38:40 -0700

    done fast

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,13 +2,10 @@
 
 /**
  * reset_to_98 - updates the value to 98
- * @n: our int
+ * @n: pointer to our int
  * Return: no return
  */
 void reset_to_98(int *n)
 {
-	int *p;
-
-	p = &n;
-	*p = 98;
+	*n = 98;
 }
