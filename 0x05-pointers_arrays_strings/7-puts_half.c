985bc2e0b96e728d18fb244f8b918e0ff689d645
MoodyEG <moodyeg2011@yahoo.com>
2023-10-17 03:18:55 -0700

    done fast

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: pointer to our char
+ * Return: no return
+ */
+void puts_half(char *str)
+{
+	int a = 0;
+	int b;
+
+	while (str[a] != '\0')
+	{
+		a++;
+	}
+	b = a / 2;
+	while (b < a)
+	{
+		_putchar(str[b]);
+		b++;
+	}
+	_putchar('\n');
+}
