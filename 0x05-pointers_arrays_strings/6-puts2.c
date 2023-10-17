93529e54c5757715dde0aa671414b9133141ee32
MoodyEG <moodyeg2011@yahoo.com>
2023-10-17 02:59:35 -0700

    done fast

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: pointer to our char
+ * Return: no return
+ */
+void puts2(char *str)
+{
+	int a = 0; /*our length*/
+
+	while (s[a] != '\0')
+	{
+		if (a % == 0)
+		{
+		_putchar(a);
+		}
+		a++;
+	}
+}
