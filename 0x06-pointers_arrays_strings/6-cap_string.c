5fba78f34951e63b0416f721a760fd9a96a3d72c
MoodyEG <moodyeg2011@yahoo.com>
2023-10-18 13:56:19 -0700

    done fast

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,12 +9,17 @@ char *cap_string(char *str)
 {
 	int i = 0;
 
+	if (str[i] >= 'a' && str[i] <= 'z')
+	{
+		str[i] = str[i]- 'a' + 'A';
+	}
 	while (str[i] != '\0')
 	{
 		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
 		str[i] == ',' || str[i] == ';' || str[i] == '.' ||
 		str[i] == '!' || str[i] == '?' || str[i] == '"' ||
-		str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+		str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+		str[i] == '}')
 		{
 			i++;
 			if (str[i] >= 'a' && str[i] <= 'z')
