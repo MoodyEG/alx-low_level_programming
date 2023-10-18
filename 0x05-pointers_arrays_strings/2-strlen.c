a6daf88e5e35602796ddb2025ff239e71aade4f5
MoodyEG <moodyeg2011@yahoo.com>
2023-10-18 14:11:55 -0700

    done fast

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,35 +1,15 @@
 #include "main.h"
 
 /**
- * *cap_sing - capitalizes all words of a sing
- * @s: our pointer to char
- * Return: pointer to the result
+ * _strlen - a function that returns the length of a string
+ * @s: pointer to our char
+ * Return: length of a string
  */
 int _strlen(char *s)
 {
-	int i = 0;
+		int a = 0;
 
-	if (s[i] >= 'a' && s[i] <= 'z')
-	{
-		s[i] = s[i]- 'a' + 'A';
-	}
-	while (s[i] != '\0')
-	{
-		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' ||
-		s[i] == ',' || s[i] == ';' || s[i] == '.' ||
-		s[i] == '!' || s[i] == '?' || s[i] == '"' ||
-		s[i] == '(' || s[i] == ')' || s[i] == '{' ||
-		s[i] == '}')
-		{
-			i++;
-			if (s[i] >= 'a' && s[i] <= 'z')
-			{
-				s[i] = s[i]- 'a' + 'A';
-			}
-			i--;
-		}
-		i++;
-	}
-
-	return (1);
+		while (*(s + a) != '\0')
+				a++;
+		return (a);
 }
