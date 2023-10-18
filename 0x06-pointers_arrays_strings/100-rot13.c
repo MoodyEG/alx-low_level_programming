04ca915d91f377cff31c926bcd36da36cea5bcf6
MoodyEG <moodyeg2011@yahoo.com>
2023-10-18 15:32:50 -0700

    done fast

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *leet - encodes a string using rot13
+ * @c: our pointer to char
+ * Return: pointer to the result
+ */
+char *rot13(char *c);
+{
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int e;
+	char *z = c;
+
+	while (*c)
+	{
+		for (e = 0; e < 52; e++)
+		{
+			if (*c == a[e])
+			{
+				*c = b[e];
+				break;
+			}
+		}
+		c++;
+	}
+
+	return (z);
+}
