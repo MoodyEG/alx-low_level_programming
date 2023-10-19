35d55531bf62e8fc9a41d5bc2fe0a52870ad4316
MoodyEG <moodyeg2011@yahoo.com>
2023-10-19 06:46:55 -0700

    done fast

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -10,46 +10,31 @@
 void print_buffer(char *b, int size)
 {
 	int i, j;
-/*had a lot of help from chatGPT in this one*/
+
 	for (i = 0; i < size; i += 10)
 	{
 		printf("%08x: ", i);
 		for (j = 0; j < 10; j++)
 		{
 			if (i + j < size)
-			{
 				printf("%02x", b[i + j]);
-			}
 			else
-			{
 				printf("  ");
-			}
 
 			if (j % 2 == 1)
-			{
 				printf(" ");
-			} 
 		}
 		for (j = 0; j < 10; j++)
 		{
 			if (i + j < size && (b[i + j] >= 32 && b[i + j] <= 126))
-			{
 				printf("%c", b[i + j]);
-			}
 			else if (i + j < size)
-			{
 				printf(".");
-			}
 			else
-			{
 				printf(" ");
-			}
 		}
 		printf("\n");
 	}
-
 	if (size <= 0)
-	{
 		printf("\n");
-	}
 }
