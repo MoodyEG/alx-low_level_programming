e072a927adbdf408b314a7c613cfa4381660bc3f
MoodyEG <moodyeg2011@yahoo.com>
2023-10-23 05:48:24 -0700

    done fast

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,7 +4,7 @@
  * *_memset -  fills memory with a constant byte
  * @b: The buffer to be printed.
  * @n: The number to fill
- * @*s: main pointer
+ * @s: main pointer
  * Return: a pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
