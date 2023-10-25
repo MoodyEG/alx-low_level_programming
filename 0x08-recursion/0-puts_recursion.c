243e8815a05b8bfb1d270b97ba5889205425ce2d
MoodyEG <moodyeg2011@yahoo.com>
2023-10-25 07:36:52 -0700

    done fast

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts_recursion -  prints a string, followed by a new line
+ * @s: our string
+ * Return: no return
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	s++;
+	_puts_recursion(*s);
+}
