ba460804a5c6f6cc21c0b2b398b07ba06da09a50
MoodyEG <moodyeg2011@yahoo.com>
2023-10-25 07:41:25 -0700

    done fast

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,7 +7,14 @@
  */
 void _puts_recursion(char *s)
 {
-	_putchar(s);
-	s++;
-	_puts_recursion(s);
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
