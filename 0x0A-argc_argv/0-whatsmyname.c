ebd04d07664fa3886f9f8688da4c10884d637dcf
MoodyEG <moodyeg2011@yahoo.com>
2023-10-27 14:16:55 -0700

    done fast

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: numbers of stuff added
+ * @argv: the things added
+ * Return: always 0 SUCCESS
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	argc++;
+	argc--;	
+
+	return (0);
+}
