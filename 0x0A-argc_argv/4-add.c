52fdf874a65a68f7509ad1c6d2c181c98d911fa9
MoodyEG <moodyeg2011@yahoo.com>
2023-10-27 14:55:08 -0700

    done fast

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ * @argc: numbers of stuff added
+ * @argv: the things added
+ * Return: always 0 SUCCESS, or 1 if error
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	for (i = 0; i < argc; i++)
+	{
+		sum += atoi(argv[i]);
+		if (argv[i] >= '0' && argv[i] <= '9')
+		{
+			printf("Error\n");
+			j = 1
+		}
+	}
+	if (argc = 1)
+	{
+		printf("0\n");
+	}
+	else if (j)
+	{
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", sum);
+	}
+
+	return (0);
+}
