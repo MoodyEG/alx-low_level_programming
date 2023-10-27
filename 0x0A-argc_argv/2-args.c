47832c4d800bd83c9c68965c56dfafa6a702cd47
MoodyEG <moodyeg2011@yahoo.com>
2023-10-27 14:22:42 -0700

    done fast

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: numbers of stuff added
+ * @argv: the things added
+ * Return: always 0 SUCCESS
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc, i++)
+	{
+	printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
