8c1ef4f62727ad147888f11f65748b19488d94cf
MoodyEG <moodyeg2011@yahoo.com>
2023-10-27 15:14:12 -0700

    done fast

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, j, sum = 0;
 
 	if (argc == 1)
 	{
@@ -19,12 +19,15 @@ int main(int argc, char *argv[])
 	}
 	for (i = 1; i < argc; i++)
 	{
-		sum += atoi(argv[i]);
-		if (*argv[i] < '0' || *argv[i] > '9')
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (*argv[i][j] < '0' || *argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
+		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 
