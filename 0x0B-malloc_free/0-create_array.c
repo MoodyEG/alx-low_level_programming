5f458fbcee5bff150afa9ec2f718c770853a2f5b
MoodyEG <moodyeg2011@yahoo.com>
2023-10-30 14:39:19 -0700

    done fast

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 	{
 		return ('\0');
 	}
-	*a = malloc(size * sizeof(char));
+	*a = char* malloc(size * sizeof(char));
 	if (a == '\0')
 	{
 		return ('\0');
