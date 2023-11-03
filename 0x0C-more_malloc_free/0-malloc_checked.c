a334d7561390e1a1a8795f16817d3b1c5d24e5a4
MoodyEG <moodyeg2011@yahoo.com>
2023-11-03 04:07:14 -0700

    done fast

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,7 +10,7 @@ void *malloc_checked(unsigned int b)
 {
 	char *a;
 
-	a = malloc(b)
+	a = malloc(b);
 	if (a == '/0')
 	{
 		exit(98);
--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,27 +10,27 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int lens1, lens2, i;
+	unsigned int lens1, lens2, i, j;
 	char *s3;
 /*starting now, I will pick better names other that abcijk*/
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (lens2 = 0; s2[lens2] != '/0'; lens2++)
+	for (lens2 = 0; s2[lens2] != '\0'; lens2++)
 		;
-	for (lens1 = 0; s1[lens1] != '/0'; lens1++)
+	for (lens1 = 0; s1[lens1] != '\0'; lens1++)
 		;
 	if (n > lens2)
 		n = lens2;
 	s3 = malloc(lens1 + n + 1);
 	if (s3 == NULL)
 		return (NULL);
-	for (i = 0; s1[i] != '/0'; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 		s3[i] = s1[i];
 	for (j = 0; j < n; j++)
 		s3[i + j] = s2[j];
-	s3[i + j] = '/0';
+	s3[i + j] = '\0';
 
 	return (s3);
 }
