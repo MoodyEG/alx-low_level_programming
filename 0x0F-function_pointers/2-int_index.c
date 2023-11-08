dc52f565513148ead145d8378a3420dcd3061229
MoodyEG <moodyeg2011@yahoo.com>
2023-11-08 08:12:20 -0800

    done fast

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: the array
+ * @size: size of the array
+ * @cmp: a FUnCtIoN poniter
+ * Return: no return
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array && size && action)
+	{
+		if (size <= 0)
+			return (-1);
+
+		while (i < size)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
+	}
+
+	return (-1)
+}
+
