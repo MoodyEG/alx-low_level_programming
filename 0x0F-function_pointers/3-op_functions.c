88514411c609d19f696a6c17d52cf91fc6bbcdba
MoodyEG <moodyeg2011@yahoo.com>
2023-11-08 11:44:00 -0800

    done fast

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "calc.h"
+
+/**
+ * op_add - sums
+ * @a: our number
+ * @b: our number
+ * Return: the sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtracts
+ * @a: our number
+ * @b: our number
+ * Return: the subtraction
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies
+ * @a: our number
+ * @b: our number
+ * Return: the product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - devides
+ * @a: our number
+ * @b: our number
+ * Return: the division
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+/**
+ * op_mod - modules
+ * @a: our number
+ * @b: our number
+ * Return: the rest
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
