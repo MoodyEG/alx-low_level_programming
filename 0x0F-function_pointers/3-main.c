15d5dab9138ac37ea874617ff1bd1cc1f75e0fae
MoodyEG <moodyeg2011@yahoo.com>
2023-11-08 12:24:57 -0800

    done fast

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,6 +16,11 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
 	i = atoi(argv[1]);
 	j = atoi(argv[3]);
 	math = get_op_func(argv[2]);
