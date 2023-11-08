7eb595ecdad87a01266fdd8aea1491b0e4372ce0
MoodyEG <moodyeg2011@yahoo.com>
2023-11-08 12:20:11 -0800

    done fast

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,5 @@
-#ifndef CALC_H
-#define CALC_H
+#ifndef _CALC_H
+#define _CALC_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,8 +12,8 @@
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
@@ -23,4 +23,4 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 
-#endif /* CALC_H */
+#endif /* _CALC_H */
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+
+/**
+ * main -  main function
+ * @argc: number of arguments
+ * @argv: the arguments
+ * Return: 0 if success
+ */
+int main(int argc, char *argv[])
+{
+	int i, j;
+	int (*math)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	i = atoi(argv[1]);
+	j = atoi(argv[3]);
+	math = get_op_func(argv[2]);
+	if (math == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	printf("%d\n", math(i, j));
+
+	return (0);
+}
+
