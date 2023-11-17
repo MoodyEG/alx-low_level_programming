26b4aeed04ace3bc8839b79f41911c4532611514
MoodyEG <moodyeg2011@yahoo.com>
2023-11-16 16:51:02 -0800

    done fast

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * turtle - slow and stead wins the race
+ *
+ * Return: no return
+*/
+void __attribute__ ((constructor)) turtle(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
