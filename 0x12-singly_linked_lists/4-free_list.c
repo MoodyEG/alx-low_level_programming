848c86f16056e8cfdcf8859d030956a80d66a7ee
MoodyEG <moodyeg2011@yahoo.com>
2023-11-16 16:14:58 -0800

    done fast

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list.
+ * @head: the head od list
+ * Return: no return
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
+
