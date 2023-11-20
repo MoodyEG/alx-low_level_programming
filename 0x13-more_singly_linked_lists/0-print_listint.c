77a63e0c9fd8303d4683709911e60553f4f53d85
MoodyEG <moodyeg2011@yahoo.com>
2023-11-20 11:52:18 -0800

    done fast

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_listint - frees a list_t list.
+ * @h: the head od list
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	listint_t *temp;
+	size_t i = 0;
+
+	while (h)
+	{
+		temp = h->next;
+		printf("%d\n", h->n);
+		h = temp;
+		i++;
+	}
+
+	return (i);
+}
+
