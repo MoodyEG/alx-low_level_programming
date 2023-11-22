c5fed766dc988eb615b6371567bf53b6ac2c444c
MoodyEG <moodyeg2011@yahoo.com>
2023-11-21 18:37:31 -0800

    done fast

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,7 +11,7 @@ size_t print_listint_safe(const listint_t *head)
 	size_t i = 0;
 
 	if (!head || !(head->next))
-		exit(98);
+		return (0);
 	slow = (listint_t *)head;
 	fast = head->next;
 	while (fast != NULL && fast < slow)
