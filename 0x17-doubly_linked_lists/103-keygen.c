05a64afa9c48dad6cd416263e6327ed3009fca02
MoodyEG <moodyeg2011@yahoo.com>
2024-01-11 12:29:17 -0800

    done fast

---
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+/**
+ * main - keygen for crackme5
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char** argv)
+{
+	char *dictionary, key[6];
+	int len, i, j;
+
+	(void)argc;
+	len = strlen(argv[1]);
+	dictionary = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	key[0] = dictionary[((len ^ 0x3b) & 0x3f)];
+	for (i = 0, j = 0; i < len; i++)
+		j += argv[1][i];
+	key[1] = dictionary[((j ^ 0x4f) & 0x3f)];
+	for (i = 0, j = 1; i < len; i++)
+		j *= argv[1][i];
+	key[2] = dictionary[((j ^ 0x55) & 0x3f)];
+	for (i = 0, j = argv[1][0]; i < len; i++)
+	{
+		if (argv[1][i] > j)
+			j = argv[1][i];
+	}
+	srand((j ^ 0xe));
+	key[3] = dictionary[(rand() & 0x3f)];
+	for (i = 0, j = 0; i < len; i++)
+		j += argv[1][i] * argv[1][i];
+	key[4] = dictionary[((j ^ 0xef) & 0x3f)];
+	for (i = 0, j = 0; i < argv[1][0]; i++)
+		j = rand();
+	key[5] = dictionary[((j ^ 0xe5) & 0x3f)];
+	printf("%s", key);
+
+	return (0);
+}
