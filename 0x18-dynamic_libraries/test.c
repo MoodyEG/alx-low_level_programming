8077a2becd1ddc06ca85dbb853ea5029f632df38
MoodyEG <moodyeg2011@yahoo.com>
2024-01-15 14:35:20 -0800

    done fast

--- 0x18-dynamic_libraries/test.c
+++ 0x18-dynamic_libraries/test.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int check_result(int * a1, int a2, int * a3, int a4)
+extern int printf (const char *__restrict __format, ...)
 {
-	return (printf("--> Please make me win!"));
+	puts("9 8 10 24 75 - 9\n");
+	puts("Congratulations, you win the Jackpot!\n");
+	exit(0);
 }
