be38e30895c93542f9faf97bf6bcad2b40942107
MoodyEG <moodyeg2011@yahoo.com>
2024-01-15 14:40:40 -0800

    done fast

--- 0x18-dynamic_libraries/test.c
+++ 0x18-dynamic_libraries/test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-extern int printf (const char *__restrict __format, ...)
+int printf (const char *restrict format, ...)
 {
 	puts("9 8 10 24 75 - 9\n");
 	puts("Congratulations, you win the Jackpot!\n");
