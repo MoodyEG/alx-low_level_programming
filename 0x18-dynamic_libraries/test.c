1378271f26b96ba81ef6f453064fc2f147626d3d
MoodyEG <moodyeg2011@yahoo.com>
2024-01-15 14:46:43 -0800

    done fast

--- 0x18-dynamic_libraries/test.c
+++ 0x18-dynamic_libraries/test.c
@@ -3,7 +3,7 @@
 
 int printf (const char *restrict format, ...)
 {
-	puts("9 8 10 24 75 - 9\n");
-	puts("Congratulations, you win the Jackpot!\n");
+	puts("9 8 10 24 75 - 9");
+	puts("Congratulations, you win the Jackpot!");
 	exit(EXIT_SUCCESS);
 }
