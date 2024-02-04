c4bba4071575d591f96e5167395bf4258b5208c1
MoodyEG <moodyeg2011@yahoo.com>
2024-02-04 09:48:04 -0800

    done fast

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table.
+ * @ht: the hash table you want to add or update the key/value to
+ * Return: no return
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int index;
+	hash_node_t *temp;
+	char *a = "";
+
+	if (ht == NULL)
+	{
+		printf("{}\n");
+		return;
+	}
+	printf("{");
+	for (index = 0; index < ht->size; index++)
+	{
+		temp = ht->array[index];
+		while (temp)
+		{
+			printf("%s'%s': '%s'", a, temp->key, temp->value);
+			a = ", ";
+			temp = temp->next;
+			if (temp)
+				printf(", ");
+		}
+	}
+	printf("}\n");
+}
